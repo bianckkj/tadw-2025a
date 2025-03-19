769397f4d0c6346ca2949c34ab66ec7d520eb1ab
bianckkj <biancaavitoria1236@gmail.com>
2025-03-19 08:42:37 -0300

    reorganizei os arquivos

--- codigo/listarClientes.php
+++ codigo/listarClientes.php
@@ -1,6 +1,3 @@
-<?php
-require_once "conexao.php";
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,35 +8,47 @@ require_once "conexao.php";
 </head>
 
 <body>
-    <h1>Lista de Clientes</h1>
+    <h1>Lista de clientes</h1>
 
-    <table>
-        <tr>
-            <td>Id. Cliente</td>
-            <td>Nome</td>
-            <td>CPF</td>
-            <td>Endereço</td>
-        </tr>
+    <?php
+    require_once "conexao.php";
+    require_once "funcoes.php";
+
+    $lista_clientes = listarClientes($conexao);
+    
+    //verificar se encontrou clientes antes de imprimir.
+    if (count($lista_clientes) == 0) {
+        echo "Não existem clientes cadastrados.";
+    } else {
+    ?>
+        <table border="1">
+            <tr>
+                <td>Id</td>
+                <td>Nome</td>
+                <td>CPF</td>
+                <td>Endereço</td>
+                <td colspan="2">Ação</td>
+            </tr>
+
         <?php
-        
-        $sql = "SELECT * FROM tb_cliente";
-        $resultados = mysqli_query($conexao, $sql);
+        foreach ($lista_clientes as $cliente) {
+            $idcliente = $cliente['idcliente'];
+            $nome = $cliente['nome'];
+            $cpf = $cliente['cpf'];
+            $endereco = $cliente['endereco'];
 
-        while ($linha = mysqli_fetch_array($resultados)) {
-            $id = $linha['idcliente'];
-            $nome = $linha['nome'];
-            $cpf = $linha['cpf'];
-            $endereco = $linha['endereco'];
-            
             echo "<tr>";
-            echo "<td>$id</td>";
+            echo "<td>$idcliente</td>";
             echo "<td>$nome</td>";
             echo "<td>$cpf</td>";
             echo "<td>$endereco</td>";
+            echo "<td><a href='formCliente.php?id=$idcliente'>Editar</a></td>";
+            echo "<td><a href='deletarCliente.php?id=$idcliente'>Excluir</a></td>";
             echo "</tr>";
         }
+    }
         ?>
-    </table>
+        </table>
 </body>
 
 </html>
